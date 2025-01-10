0d6e5f8ec42f966ba56d20d7cab5410a277eafe6
YeThura-424 <rathanpoudle.26@gmail.com>
2025-01-10 22:14:18 +0630

    setup database with appwrite and query

---
+++ src/appwrite/database.js
@@ -0,0 +1,42 @@
+import conf from "../conf/conf";
+import { Client, Databases, Storage, Query, ID } from "appwrite";
+
+export class Database {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log("Appwrite service :: getPost() :: ", error);
+      return false;
+    }
+  }
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log("Appwrite service :: getPosts() :: ", error);
+      return false;
+    }
+  }
+}
