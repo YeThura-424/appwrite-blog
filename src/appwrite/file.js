d8bd9836a63ed67c85a0e3f0747d04adcc7d5523
YeThura-424 <rathanpoudle.26@gmail.com>
2025-01-11 21:38:52 +0630

    services for appwrite file

---
+++ src/appwrite/file.js
@@ -0,0 +1,40 @@
+import config from "../config";
+import { Client, Storage, ID } from "appwrite";
+
+export class FileService {
+  client = new Client();
+  bucket;
+
+  constructor() {
+    this.client.setEndpoint(config.projectUrl).setProject(config.projectId);
+
+    this.bucket = new Storage(this.client);
+  }
+
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(config.bucketId, ID.unique(), file);
+    } catch (error) {
+      console.log("Appwrite service :: uploadFile() :: ", error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      return await this.bucket.deleteFile(config.bucketId, fileId);
+    } catch (error) {
+      console.log("Appwrite service :: deleteFile() :: ", error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId) {
+    try {
+      return this.bucket.getFilePreview(config.bucketId, fileId);
+    } catch (error) {
+      console.log("Appwrite service :: getFilePreview() :: ", error);
+      return false;
+    }
+  }
+}
