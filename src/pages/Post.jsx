56550d2c188e32c767e317e13ef05182d5f89bce
YeThura-424 <rathanpoudle.26@gmail.com>
2025-01-14 21:07:08 +0630

    single post component

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router";
+import database from "../appwrite/database";
+import file from "../appwrite/file";
+import Container from "../components/container/Container";
+import Button from "../components/Button";
+import parse from "html-react-parser";
 
 const Post = () => {
-  return <div>Post</div>;
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.auth.userData);
+
+  const isAuthor = post && userData ? post.userId === userData.$id : false;
+
+  useEffect(() => {
+    database.getPost(slug).then((post) => {
+      if (post) {
+        setPost(post);
+      } else {
+        navigate("/");
+      }
+    });
+  }, [slug, navigate]);
+
+  const deletePost = () => {
+    database.deletePost(post.$id).then((status) => {
+      if (status) {
+        file.deleteFile(post.featuredImage);
+        navigate("/");
+      }
+    });
+  };
+  return post ? (
+    <div className="py-8">
+      <Container>
+        <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
+          <img
+            src={file.getFilePreview(post.featuredImage)}
+            alt="file image"
+            className="rounded-xl"
+          />
+          {isAuthor && (
+            <div className="absolute right-6 top-6">
+              <Link to={`/edit-post/${post.$id}`}>
+                <Button bgColor="bg-green-500" className="mr-3">
+                  Edit
+                </Button>
+              </Link>
+              <Button bgColor="bg-red-500" onClick={deletePost}>
+                Delete
+              </Button>
+            </div>
+          )}
+        </div>
+        <div className="w-full mb-6">
+          <h1 className="text-2xl font-bold">{post.title}</h1>
+          <div className="browser-css">{parse(post.content)}</div>
+        </div>
+      </Container>
+    </div>
+  ) : null;
 };
 
 export default Post;
