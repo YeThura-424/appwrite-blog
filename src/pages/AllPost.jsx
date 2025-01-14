0908852f1e5ac30ac843f2b1f9e07023e6e9c24e
YeThura-424 <rathanpoudle.26@gmail.com>
2025-01-14 21:38:04 +0630

    post add, edit and list

--- src/pages/AllPost.jsx
+++ src/pages/AllPost.jsx
@@ -1,7 +1,31 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import databaseService from "../appwrite/database";
+import Container from "../components/container/Container";
 const AllPost = () => {
-  return <div>AllPost</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    databaseService.getPosts([]).then((posts) => {
+      if (posts) {
+        // console.log(posts , 'response')
+        setPosts(posts.documents); // return ka documents nat lar loh
+      }
+    });
+  }, []);
+
+  return (
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div className="p-2 w-1/4" key={post.$id}>
+              <PostCard {...post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default AllPost;
--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import databaseService from "../appwrite/database";
+import Container from "../components/container/Container";
+import PostForm from "../components/post-form/PostForm";
 
 const EditPost = () => {
-  return <div>EditPost</div>;
+  const [post, setPost] = useState();
+  const navigate = useNavigate();
+  const { slug } = useParams();
+
+  useEffect(() => {
+    databaseService.getPost(slug).then((post) => {
+      if (post) {
+        setPost(post);
+      } else {
+        navigate("/");
+      }
+    });
+  }, [slug, navigate]);
+
+  return (
+    <div className="py-6">
+      <Container>
+        <PostForm post={post} />
+      </Container>
+    </div>
+  );
 };
 
 export default EditPost;
