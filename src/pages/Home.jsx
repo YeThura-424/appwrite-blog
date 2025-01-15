cb88d48f0e1e68504eaec97ee5e7dc374fb22a4a
YeThura-424 <rathanpoudle.26@gmail.com>
2025-01-15 21:35:39 +0630

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import databaseService from "../appwrite/database";
+import Container from "../components/container/Container";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    databaseService.getPosts([]).then((posts) => {
+      if (posts) {
+        // console.log(posts , 'response')
+        setPosts(posts.documents); // return ka documents nat lar loh
+      }
+    });
+  }, []);
+
+  if (posts.length === 0) {
+    return (
+      <div className="w-full py-8">
+        <Container>
+          <div className="flex flex-wrap">
+            <h1>Login to read posts</h1>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div className="p-2 w-1/4" key={post.$id}>
+              <PostCard {...post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Login as LoginForm } from "../components";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="py-8">
+      <LoginForm />
+    </div>
+  );
 };
 
 export default Login;
